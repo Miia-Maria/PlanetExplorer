dcc4e6e09a5393adb2f3cb60e702798dc3889bc2
somename <someemail>
2015-10-23 12:19:18 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -2,12 +2,19 @@ package org.unioulu.tol.sqat2015.planetExplorer.tests;
 
 import static org.junit.Assert.*;
 
+
 import org.junit.Test;
+import org.unioulu.tol.sqat2015.planetExplorer.PlanetExplorer;
 
 public class TestPlanetExplorer {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testStartFacingisNorth() {
+		//arrange
+		PlanetExplorer planetExplorer = new PlanetExplorer(0,0, "North");
+		//act
+		planetExplorer.position(0,0, ""); 
+		//assert
+		assertEquals(planetExplorer, planetExplorer.getFacing());
 	}
 }
