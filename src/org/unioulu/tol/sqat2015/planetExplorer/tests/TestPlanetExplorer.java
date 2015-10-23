2e57a2190be0d79e8886de148d9608717b05d178
somename <someemail>
2015-10-23 13:26:34 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.unioulu.tol.sqat2015.planetExplorer.PlanetExplorer;
 
 public class TestPlanetExplorer {
+		
 	
 	@Test
 	public void testIfFacingNorthCommandLeftChangeItWest(){
@@ -31,26 +32,26 @@ public class TestPlanetExplorer {
 	}
 	
 	@Test
-	public void testIfFacingNorthCommandForwardReminesNorthAndXChanges(){
+	public void testIfFacingNorthCommandForwardReminesNorthAndXPositionChanges(){
 		PlanetExplorer planetExplorer = new PlanetExplorer(0,0,"North");
 		
 		planetExplorer.executeCommand("f");
 
 		
 		assertEquals("North",planetExplorer.getFacing());
-		assertEquals(1,planetExplorer.getX());
+		assertEquals(1,planetExplorer.getPos_X());
 		
 	}
 	
 	@Test
-	public void testIfFacingNorthCommandBacwardReminesNorthAndXChanges(){
+	public void testIfFacingNorthCommandBacwardReminesNorthAndXPositionChanges(){
 		PlanetExplorer planetExplorer = new PlanetExplorer(0,0,"North");
 		
 		planetExplorer.executeCommand("b");
 
 		
 		assertEquals("North",planetExplorer.getFacing());
-		assertEquals(-1,planetExplorer.getX());
+		assertEquals(-1,planetExplorer.getPos_X());
 		
 	}
 	
